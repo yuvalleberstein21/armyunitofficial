0027cab07b6da7e5039c9e7a9edeb94eedd62cdb
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-11-21 17:11:54 +0200

    update

--- client/src/layouts/Banner.jsx
+++ client/src/layouts/Banner.jsx
@@ -25,9 +25,9 @@ const Banner = () => {
             {/* Main Content Container */}
             <div className="flex flex-col flex-grow">
                 {/* Top 75% - Title Section */}
-                <div className="flex-grow flex items-center justify-center">
+                <div className="flex-grow flex items-center justify-center mt-8">
                     <div className="text-center px-4">
-                        <h1 className="text-5xl font-semibold text-primary sm:text-5xl text-banner">
+                        <h1 className="text-4xl font-semibold text-primary sm:text-5xl text-banner">
                             {t('pereh association')}
                         </h1>
                         <p className="text-lg py-3 md:text-2xl font-medium text-primary sm:text-xl/8 second_textBanner" dir="rtl">
@@ -37,11 +37,11 @@ const Banner = () => {
                 </div>
 
                 {/* Bottom 25% - Description and Logo */}
-                <div className="h-2/5 bg-accent-light bg-opacity-100 lg:bg-opacity-90 py-10 mx-0 md:mx-0 lg:mx-28">
+                <div className="h-2/5 bg-accent-light bg-opacity-100 lg:bg-opacity-90 py-6 mx-0 md:mx-0 lg:mx-28">
                     <div className="flex items-center max-w-6xl mx-auto h-full" dir={isRtl ? 'rtl' : 'ltr'}>
 
                         <div className="w-full lg:w-3/4 px-5">
-                            <h2 className="text-gray-800 font-normal leading-relaxed text-sm md:text-base lg:text-base">
+                            <h2 className="text-gray-800 font-normal leading-relaxed text-md md:text-base lg:text-base">
                                 {t('pereh Association works to provide financial, reputational, and operational support to the Pereh Unit, with the goal of strengthening its independence and enhancing its capabilities to protect the residents of the Golan Heights. With your donation, we can continue to strengthen the unit and ensure the safety of Golan residents. Join us and take part in protecting the Golan Heights and ensuring the security of its residents.')}
                             </h2>
                         </div>
