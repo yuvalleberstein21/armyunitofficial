0dd88795681142f62e12676dfb66dc02009c70a3
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-11-23 21:14:44 +0200

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import './helpers/i18n';
 import MainContent from './MainContnent';
 import AccessibilityWidget from './components/AccessibilityWidget';
 import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 
 
 
@@ -11,20 +12,20 @@ import { useTranslation } from 'react-i18next';
 
 function App() {
   const { t } = useTranslation();
+  useEffect(() => {
+    document.getElementById('root').classList.add('loaded');
+  }, []);
   return (
     <div className="relative min-h-screen">
       {/* Accessibility Widget - Positioned Absolutely */}
       <div className="fixed top-4 left-4 z-50">
         <AccessibilityWidget />
       </div>
-
       <Header />
       <MainContent />
-
       <footer>
         <div className="p-5 items-center bg-accent-dark">
           <span className="w-full flex justify-center text-center font-light text-primary">
-            {/* {t(`supporting the Golan's first line defense`)} */}
             {t("© 2024 All rights reserved. Pereh Association")}
           </span>
         </div>
