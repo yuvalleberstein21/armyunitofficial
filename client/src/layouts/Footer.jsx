35b92cdd71dc5a43a56c04cd584544cd5050797f
MailBIT Sales <yuvalleberstein21@gmail.com>
2025-04-16 20:58:19 +0300

    update changes 2025

--- client/src/layouts/Footer.jsx
+++ client/src/layouts/Footer.jsx
@@ -1,14 +1,14 @@
 import { useTranslation } from "react-i18next";
 
 
+
 const Footer = () => {
     const { t } = useTranslation();
     return (
         <footer>
-            <div className="p-5 items-center bg-accent-dark">
-                <span className="w-full flex justify-center text-center font-light text-primary">
-                    {t("© 2024 All rights reserved. Pereh Association")}
-                </span>
+            <div className="p-5 flex flex-col items-center bg-accent-dark text-primary text-center font-light">
+                <span>{t("© 2025 All rights reserved. Pereh Association")}</span>
+                <a href="https://www.yuvalleberstein.dev" className="text-blue-500">Made with ❤️ by Yuval Leberstein</a>
             </div>
         </footer>
     )
